7b83e5e995aa6bc0aac120b7eace0613b06aaa55
Einwegartikel <einwegartikel7@gmail.com>
2026-06-11 22:57:50 -0400

    Git test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main() {
 	string name;
 	cin >> name;
-	cout << "Hello " << name << "!!!!!!!!!!!!" << endl;
+	cout << "Wassup " << name << "!!!!!!!!!!!!" << endl;
 	return 0;
 }
